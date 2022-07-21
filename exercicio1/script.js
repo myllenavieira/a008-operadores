0f0e871b481f27990f2a4c8d8d6c319de7addfe9
myllenavieira <vieirasilvamyllena@gmail.com>
2022-07-21 19:01:10 -0300

    Exercício 1

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+const numero = + prompt("Insira um número par");
+resto = numero % 2;
+console.log(resto);
+
+// Quando executado com números pares o resto da divisão sempre será zero, 
+// já que todo número par divido por dois será zero
+// Já se inserirmos um número ímpar, o resto será diferente de zero.
